899eea2195ac2d7030631e1a9198bde35329367d
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2022-12-29 21:41:03 +0500

    Added Logic.py

---
+++ fizzbuzz_Python/fizzbuzz_Python/Logic.py
@@ -0,0 +1,21 @@
+import sys
+from math import cos, radians
+
+class logic:
+ 
+ 
+ for x in range(maxCount):
+    
+        result.Append(calculation(x,fizzId,buzzId)
+        
+
+    def calculation(x,fizzId,buzzId):
+
+        if (value% fizzId == 0) and (value% buzzId == 0):
+        return fizzbuzz
+
+        elif (value% fizzId == 0):
+        return fizz
+
+        else (value% buzzId == 0):
+        return buzz
--- fizzbuzz_Python/fizzbuzz_Python/Program.py
+++ fizzbuzz_Python/fizzbuzz_Python/Program.py
@@ -1,1 +1,16 @@
+import sys
+from math import cos, radians
 
+class Program:
+
+    fizzId, buzzId, maxCount = map (int,input("Enter fizzId, buzzId, maxCount").split())
+    
+    #if input!=3:
+    # print("Enter 3 values")
+
+    result = []
+    
+    
+        
+
+    
