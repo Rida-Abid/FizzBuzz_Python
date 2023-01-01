fd8b7183342f5b8e9c176690e7cc135f3aea7596
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-01 21:50:28 +0500

    Added 'if statements' for if user enters negative values as input

--- fizzbuzz_Python/fizzbuzz_Python/Program.py
+++ fizzbuzz_Python/fizzbuzz_Python/Program.py
@@ -1,3 +1,4 @@
+import sys
 from Logic import logic
 
 class Program:
@@ -5,14 +6,15 @@ class Program:
     fizzId, buzzId, maxCount = map (int,input("Enter fizzId, buzzId, maxCount").split())
     
 
-   # if (input == 3):
-    results = logic.process( fizzId, buzzId, maxCount )
-    for result in results:
+    if len(sys.argv) <3:
+        print("Enter 3 values")
     
-        print (result)
+    else:
+        results = logic.process( fizzId, buzzId, maxCount )
+        for result in results:
     
-    #else:
-       # print("Enter 3 values")
+            print (result)
+        
           
 
     
