6e66368352bb24f314cd36a2a17cb13115d974cf
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-04 17:27:36 +0500

    Initial Commit

---
+++ RAR_Python/RAR_Python/Program.py
@@ -0,0 +1,12 @@
+
+class Program:
+    def main():
+        args = sys.args[1:]
+
+        if (len(args) != 5):
+            print ("error:enter 5 words")
+            print ("usage: enter 5 words")
+
+        else:
+            list = []
+            list = args
