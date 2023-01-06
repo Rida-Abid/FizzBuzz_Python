8c09473edc3a202d3411c933c9e872b1af3f57c8
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-06 23:58:12 +0500

    ...

--- RAR_Python/RAR_Python/Program.py
+++ RAR_Python/RAR_Python/Program.py
@@ -1,1 +1,35 @@
+import sys
+from Logic import logic
 
+def  DisplayValues (heading, Words):
+        print(heading)
+        print("-----------------------------")
+        for word in Words:
+            print(word)
+
+def main():
+    args = sys.argv[1:]
+        
+
+    if (len(args) < 1):
+        print ("error:Enter atleast 2 words")
+        print ("usage:Please enter words")
+        return 
+
+    Words = []
+    Words = args
+    objL = logic()
+    
+      
+         
+    DisplayValues("Original List",Words)
+    DisplayValues("ReversedList",objL.ReversedList(Words))
+    DisplayValues("Sorted List",objL.SortedList(Words))
+    DisplayValues("Randomised List",objL.Randomise(Words))
+
+
+    
+        
+         
+if __name__ == '__main__':
+    main()
