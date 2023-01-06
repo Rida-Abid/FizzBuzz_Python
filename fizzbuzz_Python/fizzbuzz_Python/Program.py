15494ce72f42d51a797528e912027126848ab3a9
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-06 23:45:34 +0500

    Changed the code for if user enters an alphabet or special character as input.

--- fizzbuzz_Python/fizzbuzz_Python/Program.py
+++ fizzbuzz_Python/fizzbuzz_Python/Program.py
@@ -4,15 +4,23 @@ from Logic import logic
 def main():
     args = sys.argv[1:]
 
-    if len(args) != 3:
-        print("error: missing input values")
-        print("useage: enter fizzId, buzzId, maxCount")
-    else:
-        fizzId, buzzId, maxCount = args
-        results = logic.process(int(fizzId), int(buzzId), int(maxCount))
-        for result in results:
-            print (result)
-      
+    try:
+        if len(args) != 3:
+            print("error: missing input values")
+            print("useage: enter fizzId, buzzId, maxCount")
+       
+        else:
+            fizzId, buzzId, maxCount = args
+            results = logic.process(int(fizzId), int(buzzId), int(maxCount))
+            for result in results:
+                print (result)
+    except ValueError:
+        for a in args:
+             if not (a.isdigit()) == True:
+                print("error:enter numbers ")
+        else:
+            print("")
+
 
 if __name__ == '__main__':
     main()
